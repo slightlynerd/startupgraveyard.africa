f24e8327907eaa74862db81035f597f7c2d4b456
KelvinGoboZap <kg@zaptest.com>
2023-12-18 16:16:07 +0100

    feat: add startup form toggle

--- src/components/app-footer.vue
+++ src/components/app-footer.vue
@@ -1,14 +1,22 @@
 <template>
   <footer class="container my-5">
-    <h2 class="h5">💀 startupgraveyard.africa</h2>
-    <startup-form />
-    <p class="disclaimer small mx-auto pt-5">
+    <h2 class="h6 footer-text mx-auto pt-5">💀 startupgraveyard.africa</h2>
+    <p class="footer-text small mx-auto mt-3">
       <strong>Disclaimer: </strong>
       <span>
         While this site offers valuable insights, remember it's not a definitive
         truth machine. Companies are intricate entities shaped by a web of
         long-term influences, making simple pronouncements risky.
       </span>
+    </p>
+    <p class="footer-text small mx-auto mt-3">
+      Don't see a startup on the list?
+      <a
+        data-bs-toggle="modal"
+        data-bs-target="#startupFormModal"
+        class="text-decoration-underline">
+        Make a submission.
+      </a>
     </p>
     <div class="pt-4">
       <p class="text-center small">
@@ -20,6 +28,30 @@
           GitHub.
         </a>
       </p>
+    </div>
+
+    <!-- Modal -->
+    <div
+      id="startupFormModal"
+      class="modal fade"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true">
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Add startup to list</h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <startup-form />
+          </div>
+        </div>
+      </div>
     </div>
   </footer>
 </template>
@@ -39,12 +71,14 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-p.disclaimer {
+p.footer-text,
+h2.footer-text {
   max-width: 75%;
 }
 
 @media screen and (max-width: 768px) {
-  p.disclaimer {
+  p.footer-text,
+  h2.footer-text {
     max-width: 100%;
   }
 }
