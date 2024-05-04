6c6ace011377d87a9d82b89ecf0e693b25d61113
Kelvin Gobo <gobokel@gmail.com>
2024-05-04 08:24:57 +0100

    feat: add extra table attributes

--- models/constants/sanitize-html.const.ts
+++ models/constants/sanitize-html.const.ts
@@ -1,16 +1,17 @@
 export const SANITIZE_HTML_OPTIONS = {
-  allowedTags: ['p', 'a', 'img', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'ul', 'ol', 'li', 'strong', 'em', 'strike', 'code', 'pre', 'span', 'br', 'div', 'table', 'thead', 'tbody', 'tr', 'th', 'td'],
+  allowedTags: ['p', 'a', 'img', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'ul', 'ol', 'li', 'strong', 'em', 'strike', 'code', 'pre', 'span', 'br', 'div', 'table', 'thead', 'tbody', 'tr', 'th', 'td', 'colgroup', 'col'],
   allowedAttributes: {
     a: ['href', 'name', 'target'],
     img: ['src', 'alt', 'class'],
     span: ['style'],
-    div: ['style'],
-    table: ['style'],
+    div: ['style', 'class', 'dir', 'align'],
+    table: ['style', 'class'],
     thead: ['style'],
     tbody: ['style'],
     tr: ['style'],
     th: ['style'],
-    td: ['style']
+    td: ['style'],
+    col: ['width']
   },
   allowedSchemes: ['http', 'https', 'mailto']
 };
