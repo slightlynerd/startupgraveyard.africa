a1a05d37e17b13a1a12d78181335ad9ae22803b0
Kelvin Gobo <gobokel@gmail.com>
2024-05-24 19:56:01 +0100

    feat: implement author page

---
+++ components/author-bio.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="col-md-8">
+    <img
+      class="author-image mb-3"
+      :src="author?.photoURL || 'https://placehold.co/200'"
+      :alt="`photo of ${author?.firstName} ${author?.lastName}`"
+    >
+    <h1>{{ author?.firstName }} {{ author?.lastName }}</h1>
+    <p class="lead mt-3">
+      {{ author?.bio }}
+    </p>
+    <div class="text-muted mt-3">
+      <social-icons
+        position="start"
+        :facebook="author?.facebook"
+        :x="author?.x"
+        :linkedin="author?.linkedIn"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// models
+import { type IAuthor } from '@/models';
+
+// props
+defineProps<{
+  author?: IAuthor;
+}>();
+</script>
+
+<style lang="scss" scoped>
+.author-image {
+  border-radius: 50%;
+}
+</style>
